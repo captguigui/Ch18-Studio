454684da258f057352219891be763050d51a32a0
Guillermo Frenero <g.frenero.sanchez@gmail.com>
2021-06-28 18:43:28 -0400

    first commit

---
+++ Ch18-Studio/Menu.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Ch18_Studio
+{
+    public class Menu
+    {
+        public Menu()
+        {
+        }
+    }
+}
---
+++ Ch18-Studio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch18_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
